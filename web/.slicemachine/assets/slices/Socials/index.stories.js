e3da6747aef9b224520a63da36eb72496982ff39
Nathan Sorkin <NateSrkn@gmail.com>
2022-09-07 20:53:38 -0400

    move files out of sub folder structure

--- web/.slicemachine/assets/slices/Socials/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/Socials';
-
-export default {
-  title: 'slices/Socials'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"label":[{"type":"paragraph","text":"Non ea et in dolor velit laborum minim ullamco incididunt dolor eu.","spans":[]}],"link":{"link_type":"Web","url":"http://google.com"}},"slice_type":"socials","id":"_Default"}} />
-_Default.storyName = ''
